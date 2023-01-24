cf2a409a2ec6bbe3696cde4cb0d6006b0fe39d1f
NdakoAdama <adamamuhammad01@gmail.com>
2023-01-24 07:17:36 -0800

    sorting_algorithms

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,4 @@
-#include "sort.h"
+nclude "sort.h"
 
 /**
  *  * swap_ints - Swap two integers in an array.
@@ -9,9 +9,9 @@ void swap_ints(int *a, int *b)
 {
 		int tmp;
 
-			tmp = *a;
-				*a = *b;
-					*b = tmp;
+		tmp = *a;
+		*a = *b;
+		*b = tmp;
 }
 
 
@@ -23,26 +23,26 @@ void swap_ints(int *a, int *b)
  *      */
 void bubble_sort(int *array, size_t size)
 {
-		size_t i, newsize = size;
-			int swap;
+	size_t i, newsize = size;
+	int swap;
 
-				if (array == NULL || size < 2)
-							return;
+	if (array == NULL || size < 2)
+			return;
 
-					while (newsize)
-							{
-										swap = 0;
-												for (i = 0; i < (size - 1); i++)
-															{
-																			if (array[i] > array[i + 1])
-																							{
-																												swap_ints(&array[i], &array[i + 1]);
-																																print_array(array, size);
-																																				swap = 1;
-																																							}
-																					}
-														newsize--;
-																if (swap == 0)
-																				break;
-																	}
+		while (newsize)
+		{
+			swap = 0;
+			for (i = 0; i < (size - 1); i++)
+			{			
+				if (array[i] > array[i + 1])
+				{								
+					swap_ints(&array[i], &array[i + 1]);
+					print_array(array, size)
+					swap =1:	
+				}
+			}
+			newsize--;
+			if (swap == 0)
+				break;
+		}
 }
