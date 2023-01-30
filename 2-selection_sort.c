851247d2499e4cc6fafb2cedabe28d03294bcb8d
NdakoAdama <adamamuhammad01@gmail.com>
2023-01-30 11:10:57 -0800

    sorting algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,27 +1,27 @@
 #include "sort.h"
 
 /**
- *  * swap_ints - Swap two integers in an array.
- *   * @a: The first integer to swap.
- *    * @b: The second integer to swap.
- *     */
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
 void swap_ints(int *a, int *b)
 {
-		int tmp;
+	int tmp;
 
-			tmp = *a;
-				*a = *b;
-					*b = tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
- *  * selection_sort - Sort an array of integers in ascending order
- *   *                  using the selection sort algorithm.
- *    * @array: An array of integers.
- *     * @size: The size of the array.
- *      *
- *       * Description: Prints the array after each swap.
- *        */
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
 void selection_sort(int *array, size_t size)
 {
 	int *min;
@@ -41,6 +41,6 @@ void selection_sort(int *array, size_t size)
 			swap_ints(array + i, min);
 			print_array(array, size);
 		}
-			}
+	}
 
 }
--- sort.h
+++ sort.h
@@ -1,4 +1,4 @@
-fndef SORT_H
+#ifndef SORT_H
 #define SORT_H
 
 #include <stdio.h>
@@ -9,17 +9,17 @@ fndef SORT_H
 #define DOWN 1
 
 /**
- *  * struct listint_s - Doubly linked list node
- *   *
- *    * @n: Integer stored in the node
- *     * @prev: Pointer to the previous element of the list
- *      * @next: Pointer to the next element of the list
- *       */
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
 typedef struct listint_s
 {
-		const int n;
-			struct listint_s *prev;
-				struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
